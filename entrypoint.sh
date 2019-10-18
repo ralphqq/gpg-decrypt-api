395ac9842587fdaae9e90b748a5890a142f8a7d2
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-10-18 20:18:44 +0800

    Symlinked run_tests.sh in /usr/local/bin

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,8 @@ python -c"import random; print(''.join(random.SystemRandom().\
 choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50)))"\
 ) >> .env
 
+echo Creating symlink
+ln -s /home/decrypt_msg_api/run_tests.sh /usr/local/bin/run_tests.sh
+
 echo Starting up gunicorn
 exec gunicorn -b 0.0.0.0:80 --access-logfile - --error-logfile - decryptmessage:app
